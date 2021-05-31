05acf455f57bd0529307cc78474c15f52049fa9d
cacico <83400153+cacico@users.noreply.github.com>
2021-05-31 20:30:45 +0900

    Add files via upload

---
+++ 0527/2210104072/question1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import itertools
+
+a = []
+for i in range(2, 100):
+    flag = 0
+    for j in range(2, i // 2 + 1):
+        if (i % j == 0):
+            flag = 1
+            break
+    if flag == 0:
+        a.append(i)
+#sum is 1060
+
+for j in reversed(range(0,int(sum(a)/2))):
+    for i in range(len(a)):
+        for pair in itertools.combinations(a,i):
+            if sum(pair)==j:
+                break
+        else:
+            continue
+        break
+    else:
+        continue
+    break
+
+print("SA=",end="");print(pair)
+print("SB is the other.")
+print("difference is ",end="");print(int((sum(a)/2-sum(pair))*2))
