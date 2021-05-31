5c374cbb5da24f2c557c24c485814980fc2b8b85
machi7 <45916369+machi7@users.noreply.github.com>
2021-06-01 02:52:18 +0900

    Add files via upload

--- 0527/2210104027/morikadai.py
+++ 0527/2210104027/morikadai.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
+# In[20]:
 
 
 import math
 a=[]
+sowa=0
 for i in range(2,101):
     flag=0
     for j in range(2,int(math.sqrt(i))+1):
@@ -14,20 +15,48 @@ for i in range(2,101):
             break
     if flag==0:
         a.append(i)
-print(a)
-a.reverse()
-print(a)
+        sowa+=i
+
+dp=[0 for i in range(10000)]
+tansaku=[0 for i in range(10000)]
+dp[0]=1
+sowa2=0
+for i in a:
+    sowa2+=i
+    for j in range(sowa2,-1,-1):
+        if dp[j]==1:
+            dp[j+i]=1
+            if tansaku[j+i]==0:
+                tansaku[j+i]=i
+
+
+ansmin=99999999
+anssum=-1
+for i in range(sowa):
+    if i>sowa-i:
+        break
+    if ansmin>(sowa-i)-i and dp[i]==1:
+        ansmin=(sowa-i)-i
+        anssum=i
 b=[]
 c=[]
+while anssum!=0:
+    b.append(tansaku[anssum])
+    anssum=anssum-tansaku[anssum]
+    if anssum==0:
+        break;
+
 for i in a:
-    if(sum(b)<=sum(c)):
-        b.append(i)
-    else:
+    if i not in b:
         c.append(i)
+print("S_A")
 print(b)
 print(sum(b))
+print("S_B")
 print(c)
 print(sum(c))
+
+print("dif:"+str(sum(b)-sum(c)))
 
 
 # In[ ]:
@@ -35,3 +64,11 @@ print(sum(c))
 
 
 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
