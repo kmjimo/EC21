c360cfb200b38e8b88a96dffe0367628f3fb8594
YTsuchimoto <83441055+YTsuchimoto@users.noreply.github.com>
2021-06-01 20:21:56 +0900

    Update Q1.py

--- 0527/2210104049/Q1.py
+++ 0527/2210104049/Q1.py
@@ -1,30 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-a = []
-for i in range(2, 100):
+a=[]
+for i in range(2,100):
     flag = 0
-    for j in range(2, i // 2 + 1):
-        if (i % j == 0):
+    for j in range(2,i//2+1):
+        if(i%j==0):
             flag = 1
             break
     if flag == 0:
         a.append(i)
-
+    
 a.reverse()
+
+#print(a)
 
 S__A = []
 S__B = []
 
+A , B = 0, 0
+
 for i in range(len(a)):
-    if (sum(S__A) >= sum(S__B)):
+    if(A > B):
         S__B.append(a[i])
+        B += a[i]
+        #print(S__B)
     else:
         S__A.append(a[i])
-
+        A += a[i]
+        #print(S__A)
+        
 S__A.reverse()
 S__B.reverse()
 
+print('A =', A)
+print('B =', B)        
 print('Sa =', S__A)
 print('Sb =', S__B)
-print('差の絶対値 =', abs(sum(S__A) - sum(S__B)))
+print('差の絶対値 =', abs(A - B))
